a9c3b211c0d1e1eead56ba39af848b1a3dd53d04
alexisLF <alexislefort.14@gmail.com>
2020-10-27 12:23:21 +0100

    add windows form event

---
+++ CoursCSharp/WindowsFormsAppEvent/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppEvent
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.Show();
+            f2.ClickButton += F_ClickButton;
+
+        }
+
+        private void F_ClickButton(object sender, Form2.ShowInfo e)
+        {
+            textBox1.Text = e.Infos;
+        }
+    }
+}
---
+++ CoursCSharp/WindowsFormsAppEvent/Form2.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppEvent
+{
+    public partial class Form2 : Form
+    {
+        public class ShowInfo
+        {
+            public string Infos { get; set; }
+        }
+
+        public EventHandler<ShowInfo> ClickButton;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ClickButton != null)
+                ClickButton(this, new ShowInfo { Infos = "Button 1" });
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (ClickButton != null)
+                ClickButton(this, new ShowInfo { Infos = "Button 2" });
+        }
+    }
+}
